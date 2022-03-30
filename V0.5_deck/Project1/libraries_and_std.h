48a51d6e11492e01aa638ebbac5a2e93fabe1e52
Ech0ke <99315244+Ech0ke@users.noreply.github.com>
2022-03-30 10:46:28 +0300

    Add files via upload

---
+++ V0.5_deck/Project1/libraries_and_std.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <deque>
+#include <random>
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::deque;
+using std::setw;
+using std::left;
+using std::right;
+using std::mt19937;
+using std::stringstream;
+using std::map;
+using hrClock = std::chrono::high_resolution_clock;
+typedef std::uniform_int_distribution<int>  int_distribution;
